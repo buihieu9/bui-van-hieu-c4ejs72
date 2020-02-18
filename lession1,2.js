63dea2180fb3ae699d94ba5e80e9f3db72371381
unknown <buivanhieu1871997@gmail.com>
2020-02-19 02:59:18 +0700

    homeworkday3

---
+++ lession1,2.js
@@ -0,0 +1,21 @@
+// lession1
+let a = 5;
+let b =10 ;
+let c = a ;
+a = b;
+b = c ;
+console.log('a ='+ a +' b ='+b)
+
+//2 
+
+    let a = 5;
+    let b = 6;
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    console.log(a,b)
+
+// lession2
+const s = 'Hello beauty there';
+const a = s.split(' ',3)
+console.log(a)
